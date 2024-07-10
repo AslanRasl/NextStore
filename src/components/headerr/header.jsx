e4dcc260dc07dd7a711ffa26709def959af90212
Aslan <117452702+AslanRasl@users.noreply.github.com>
2024-07-10 13:14:01 +0300

    Update header.jsx

--- src/components/headerr/header.jsx
+++ src/components/headerr/header.jsx
@@ -72,90 +72,118 @@ const Header = () => {
                                         <img className="burger" src={isMenuVisible ? closeIcon : burger} alt="burger icon" />
                                         <h4 className="bur__text">KATALOG</h4>
                                     </button>
-                                    {isMenuVisible && (
-                                        <div className="dropdown-menu">
-                                            <div className="dropdown-category">
-                                                <ul className="category-list">
-                                                    <li><a className="aks" href="#"><img src={Fire} alt="fire icon" /> Aksiya</a></li>
-                                                    <li><a href="/Main">Kompyuterlar va notebooklar</a></li>
-                                                    <li><a href="#">Telefonlar</a></li>
-                                                    <li><a href="#">Maishiy texnika</a></li>
-                                                    <li><a href="#">Erkaklar uchun kiyim</a></li>
-                                                    <li><a href="#">Ayollar uchun kiyimlar</a></li>
-                                                    <li><a href="#">Bolalar uchun </a></li>
-                                                    <li><a href="#">Avtomobil mahsulotlari</a></li>
-                                                    <li><a href="#">Go'zallik va so'glik</a></li>
-                                                    <li><a href="#">Sport va o'yin-kulgi</a></li>
-                                                    <li><a href="#">Uy hayvonlari uchun tovarlar</a></li>
-                                                    <li><a href="#">Xobbi va ijodkorlik</a></li>
-                                                    <li><a href="#">Kantselyariya buyumlari</a></li>
-                                                    <li><a href="#">Maishiy kimyo</a></li>
-                                                    <li><a href="#">Sport va o'yin-kulgilar</a></li>
-                                                    <li><a href="#">Uy hayvonlari uchun tovarlar</a></li>
-                                                    <li><a href="#">Aksesuarlar</a></li>
-                                                </ul>
-                                            </div>
-                                            <div className="category-inner">
-                                                <ul className="left-item">
-                                                    <span className="brands"><h4>Brendlar</h4></span>
-                                                    <li><a href="#">Samsung</a></li>
-                                                    <li><a href="#">Vivo</a></li>
-                                                    <li><a href="#">Tecno</a></li>
-                                                    <li><a href="#">Realme</a></li>
-                                                    <li><a href="Xiaomi">Xiaomi</a></li>
-                                                    <li><a href="#">Apple</a></li>
-                                                    <li><a href="#">Artel</a></li>
-                                                    <li><a href="#">Nokia</a></li>
-                                                    <li><a href="#">OnePlus</a></li>
-                                                    <li><a href="#">ZTE</a></li>
-                                                    <li><a href="#">Blackview</a></li>
-                                                    <li><a href="#">Oppo</a></li>
-                                                    <li><a href="#">Ergo</a></li>
-                                                    <li><a href="#">BQ</a></li>
-                                                    <li><a href="#">Huawei</a></li>
-                                                    <li><a href="#">Alcatel</a></li>
-                                                    <li><a href="#">TCL</a></li>
-                                                    <li><a href="#">Inoi</a></li>
-                                                    <li><a href="#" className="active-link">Ko’proq</a></li>
-                                                </ul>
-                                                <div className="grid-container">
-                                                    <ul> <span className="brands"><h4>Samsung Brendi</h4></span>
-                                                        <li><a href="#">S serialar</a></li>
-                                                        <li><a href="#">A serialar</a></li>
-                                                        <li><a href="#">Note serilar</a></li>
-                                                        <li><a href="#">M serialar</a></li></ul>
-                                                    <ul>
-                                                        <span className="brands"><h4>Redmi & Xiaomi</h4></span>
-                                                        <li><a href="#">S serialar</a></li>
-                                                        <li><a href="#">A serialar</a></li>
-                                                        <li><a href="#">Note serilar</a></li>
-                                                        <li><a href="#">M serialar</a></li>
-                                                    </ul>
-                                                    <ul>
-                                                        <span className="brands"><h4>Apple brendi</h4></span>
-                                                        <li><a href="#">S serialar</a></li>
-                                                        <li><a href="#">A serialar</a></li>
-                                                        <li><a href="#">Note serilar</a></li>
-                                                        <li><a href="#">M serialar</a></li>
-                                                    </ul>
-                                                    <ul>
-                                                        <span className="brands"><h4>Huawei</h4></span>
-                                                        <li><a href="#">S serialar</a></li>
-                                                        <li><a href="#">A serialar</a></li>
-                                                        <li><a href="#">Note serilar</a></li>
-                                                        <li><a href="#">M serialar</a></li>
-                                                    </ul>
-                                                    <ul>
-                                                        <span className="brands"><h4>Eng ommabop</h4></span>
-                                                        <li><a href="#">S serialar</a></li>
-                                                        <li><a href="#">A serialar</a></li>
-                                                        <li><a href="#">Note serilar</a></li>
-                                                        <li><a href="#">M serialar</a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )}
+                                                                        {isMenuVisible && ( 
+                                         <div className="dropdown-menu"> 
+                                             <div className="dropdown-category"> 
+                                                 <ul className="category-list"> 
+                                                     <li><a className="aks" href="#"><img src={Fire} alt="fire icon" /> Aksiya</a></li> 
+                                                     <li><a href="/Main">Kompyuterlar va notebooklar</a></li> 
+                                                     <li><a href="#">Telefonlar</a></li> 
+                                                     <li><a href="#">Maishiy texnika</a></li> 
+                                                     <li><a href="#">Erkaklar uchun kiyim</a></li> 
+                                                     <li><a href="#">Ayollar uchun kiyimlar</a></li> 
+                                                     <li><a href="#">Bolalar uchun </a></li> 
+                                                     <li><a href="#">Avtomobil mahsulotlari</a></li> 
+                                                     <li><a href="#">Go'zallik va so'glik</a></li> 
+                                                     <li><a href="#">Sport va o'yin-kulgi</a></li> 
+                                                     <li><a href="#">Uy hayvonlari uchun tovarlar</a></li> 
+                                                     <li><a href="#">Xobbi va ijodkorlik</a></li> 
+                                                     <li><a href="#">Kantselyariya buyumlari</a></li> 
+                                                     <li><a href="#">Maishiy kimyo</a></li> 
+                                                     <li><a href="#">Sport va o'yin-kulgilar</a></li> 
+                                                     <li><a href="#">Uy hayvonlari uchun tovarlar</a></li> 
+                                                     <li><a href="#">Aksesuarlar</a></li> 
+                                                 </ul> 
+                                             </div> 
+                                             <div className="category-inner"> 
+                                                 <ul className="left-item"> 
+                                                     <span className="brands"><h4>Brendlar</h4></span> 
+                                                     <li><a href="#">Samsung</a></li> 
+                                                     <li><a href="#">Vivo</a></li> 
+                                                     <li><a href="#">Tecno</a></li> 
+                                                     <li><a href="#">Realme</a></li> 
+                                                     <li><a href="Xiaomi">Xiaomi</a></li> 
+                                                     <li><a href="#">Apple</a></li> 
+                                                     <li><a href="#">Artel</a></li> 
+                                                     <li><a href="#">Nokia</a></li> 
+                                                     <li><a href="#">OnePlus</a></li> 
+                                                     <li><a href="#">ZTE</a></li> 
+                                                     <li><a href="#">Blackview</a></li> 
+                                                     <li><a href="#">Oppo</a></li> 
+                                                     <li><a href="#">Ergo</a></li> 
+                                                     <li><a href="#">BQ</a></li> 
+                                                     <li><a href="#">Huawei</a></li> 
+                                                     <li><a href="#">Alcatel</a></li> 
+                                                     <li><a href="#">TCL</a></li> 
+                                                     <li><a href="#">Inoi</a></li> 
+                                                     <li><a href="#" className="active-link">Ko’proq</a></li> 
+                                                 </ul> 
+                                                 <div className="grid-container"> 
+                                                     <ul> <span className="brands"><h4>Samsung Brendi</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li></ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Redmi & Xiaomi</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Apple brendi</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Huawei</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Eng ommabop</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Eng arzonlari</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Huawei mobile</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Huawei mobile</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                     <ul> 
+                                                         <span className="brands"><h4>Huawei mobile</h4></span> 
+                                                         <li><a href="#">S serialar</a></li> 
+                                                         <li><a href="#">A serialar</a></li> 
+                                                         <li><a href="#">Note serilar</a></li> 
+                                                         <li><a href="#">M serialar</a></li> 
+                                                     </ul> 
+                                                 </div> 
+                                             </div> 
+                                         </div> 
+                                     )}
                                  </li>
                                  <div className="srch">
                                 <input className="search" type="text" placeholder="Maxsulotlarni izlash" />
